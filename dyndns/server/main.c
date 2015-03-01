62ad41fbd3228a1aca3aa2815727ac4a7b94c898
kkrolikowski <kkrolikowski@gmail.com>
2015-03-01 20:36:37 +0100

    removed fork() code.

--- dyndns/server/main.c
+++ dyndns/server/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <strings.h>
 #include <string.h>
@@ -14,7 +13,6 @@ int main(int argc, char *argv[]) {
 	int status;
 	char * source_addr;
 	char * client_domain;
-	pid_t child, pid;
 	cfgdata_t cf;
 
 	source_addr = (char *) malloc(16 * sizeof(char));
@@ -22,46 +20,37 @@ int main(int argc, char *argv[]) {
 	bzero(source_addr, 16 * sizeof(char));
 	bzero(client_domain, 64 * sizeof(char));
 
-	if(argc < 3) {
+	if (argc < 3) {
 		fprintf(stderr, "Usage: %s [port number] <bind zonefile>\n", argv[0]);
 		exit(1);
 	}
 	sockfd = bindToInterface(atoi(argv[1]));
-	if(sockfd < 0) {
+	if (sockfd < 0) {
 		fprintf(stderr, "Cannot bind to interface\n");
 		exit(1);
 	}
-	child = fork();
-	if(!child) {
-		if((cli_fd = clientConn(sockfd, source_addr)) < 0) {
-			fprintf(stderr, "connection failed\n");
+	if ((cli_fd = clientConn(sockfd, source_addr)) < 0) {
+		fprintf(stderr, "connection failed\n");
+		exit(1);
+	} else {
+		strcpy(cf.ip_addr, source_addr);
+		if (readData(cli_fd, client_domain) > 0)
+			stripSubDomain(client_domain, cf.subdomain);
+		else {
+			fprintf(stderr, "Error reading data from client\n");
 			exit(1);
 		}
-		else {
-			strcpy(cf.ip_addr, source_addr);
-			if(readData(cli_fd, client_domain) > 0)
-				stripSubDomain(client_domain, cf.subdomain);
-			else {
-				fprintf(stderr, "Error reading data from client\n");
-				exit(1);
-			}
-		}
-		if(!fork()) {
-			printf("Connection from: %s\n", cf.ip_addr);
-			printf("Domena: %s\n", cf.subdomain);
-		}
-		pid = wait(&status);
-		printf("Proces: %d zakonczyl sie z kodem: %d\n", pid, WEXITSTATUS(status));
 	}
+	printf("Connection from: %s\n", cf.ip_addr);
+	printf("Domena: %s\n", cf.subdomain);
 	return 0;
 }
 static void stripSubDomain(char *str, char *sd) {
-	while(*str != '\0') {
-		if(*str == '.') {
+	while (*str != '\0') {
+		if (*str == '.') {
 			*sd = '\0';
 			break;
-		}
-		else
+		} else
 			*sd = *str;
 		str++;
 		sd++;

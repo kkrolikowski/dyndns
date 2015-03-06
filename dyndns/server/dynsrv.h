cb76ff40d1822f6fac4a050df1e1fb1e9a351672
kkrolikowski <kkrolikowski@gmail.com>
2015-03-06 23:16:30 +0100

    interface updated

--- dyndns/server/dynsrv.h
+++ dyndns/server/dynsrv.h
@@ -11,9 +11,10 @@ typedef struct cfgdata {
 int bindToInterface(int portno);
 int clientConn(int fd, char * cliaddr);
 int readData(int fd, char * domain);
-int updateZone(cfgdata_t * cf, char * file, int new);
+int updateZone(cfgdata_t * cf, char * file);
 int updateSerialNo(char * fromfile, char * newserial);
 void RandomFilename(char *filename);
 void stripSerialNo(char *in, char *out);
 bool if_Exist(char *item, char *zfname);
+int NewEntry(cfgdata_t * cf, char * file);
 #endif

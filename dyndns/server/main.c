835f66cd91e42608345fe0ff3356a13f47b66ee4
kkrolikowski <kkrolikowski@gmail.com>
2015-03-07 22:17:07 +0100

    wrong parameter, addedd file perms.

--- dyndns/server/main.c
+++ dyndns/server/main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-	log_fd = open(argv[3], O_RWD|O_CREAT|O_APPEND);
+	log_fd = open(argv[3], O_RDWR|O_CREAT|O_APPEND, 0644);
 	while(1) {
 		ddserv(argv[1], argv[2], int logfd);
 	}

fdb8c7c6cc40bd798e68080c691f5cab9721feb3
kkrolikowski <kkrolikowski@gmail.com>
2015-03-07 21:01:33 +0100

    our daemon doesn't work as he should. We need to do it a little bit different.

--- dyndns/server/main.c
+++ dyndns/server/main.c
@@ -12,12 +12,10 @@ static void splitDomain(char *userdomain, cfgdata_t * cfg);
 
 int main(int argc, char *argv[]) {
 	int sockfd, cli_fd;
-	int status;
 	char * source_addr;
 	char * client_domain;
 	char zonepath[64];
 	cfgdata_t cf;
-	pid_t chld;
 
 	source_addr = (char *) malloc(16 * sizeof(char));
 	client_domain = (char *) malloc(64 * sizeof(char));
@@ -28,49 +26,37 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Usage: %s [port number] <zonedir>\n", argv[0]);
 		exit(1);
 	}
-	chld = fork();
-	if(chld < 0) {
-		perror("fork");
-		exit(-1);
+	strcpy(zonepath, argv[2]);
+	sockfd = bindToInterface(atoi(argv[1]));
+	if (sockfd < 0) {
+		fprintf(stderr, "Cannot bind to interface\n");
+		exit(1);
 	}
-	if(chld > 0) {
-		printf("Starting server process\n");
-		waitpid(chld, &status, WNOHANG);
+	if ((cli_fd = clientConn(sockfd, source_addr)) < 0) {
+		fprintf(stderr, "connection failed\n");
+		exit(1);
 	}
 	else {
-		strcpy(zonepath, argv[2]);
-		sockfd = bindToInterface(atoi(argv[1]));
-		if (sockfd < 0) {
-			fprintf(stderr, "Cannot bind to interface\n");
-			exit(1);
-		}
-		if ((cli_fd = clientConn(sockfd, source_addr)) < 0) {
-			fprintf(stderr, "connection failed\n");
-			exit(1);
+		strcpy(cf.ip_addr, source_addr);
+		if (readData(cli_fd, client_domain) > 0) {
+			splitDomain(client_domain, &cf);
+			strcat(zonepath, cf.domain);
 		}
 		else {
-			strcpy(cf.ip_addr, source_addr);
-			if (readData(cli_fd, client_domain) > 0) {
-				splitDomain(client_domain, &cf);
-				strcat(zonepath, cf.domain);
-			}
-			else {
-				fprintf(stderr, "Error reading data from client\n");
-				exit(1);
-			}
+			fprintf(stderr, "Error reading data from client\n");
+			exit(1);
 		}
-		if(if_Exist(cf.subdomain, zonepath) == true) {
-			if(if_Exist(cf.ip_addr, zonepath) == false) {
-				updateZone(&cf, zonepath);
-			}
-		}
-		else
-			NewEntry(&cf, zonepath);
-
-		free(source_addr);
-		free(client_domain);
-		exit(0);
 	}
+	if(if_Exist(cf.subdomain, zonepath) == true) {
+		if(if_Exist(cf.ip_addr, zonepath) == false) {
+			updateZone(&cf, zonepath);
+		}
+	}
+	else
+		NewEntry(&cf, zonepath);
+
+	free(source_addr);
+	free(client_domain);
 	return 0;
 }
 static void splitDomain(char *userdomain, cfgdata_t * cfg) {

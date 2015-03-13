4154300ae9b8757aad962db43fe641a5d2d38ee7
Krzysztof Królikowski <kkrolikowski@gmail.com>
2015-03-13 19:22:17 +0100

    client daemonize, hardcoded update interval

--- src/client.c
+++ src/client.c
@@ -5,12 +5,15 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
 #include "config.h"
 
 int main(int argc, char *argv[]) {
 	config_t config;
+	pid_t pid, sess;
 	int sockfd, portno, n;
 	struct sockaddr_in srv_addr;
 	struct hostent *server;
@@ -28,29 +31,48 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Error reading file: %s\n", argv[2]);
 		exit(1);
 	}
+	pid = fork();
+	if(pid < 0) {
+		perror("fork");
+		exit(-1);
+	}
+	if(pid > 0) {
+		printf("Starting DynDNS client, PID: %d\n", pid);
+		exit(0);
+	}
+	umask(0);
+	sess = setsid();
+	chdir("/");
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 	portno = config.port;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0)
-		perror("Error opening socket");
-	server = gethostbyname(config.client.host);
-	if(server == NULL) {
-		fprintf(stderr, "Unknown host\n");
-		exit(1);
-	}
-	bzero((char *) &srv_addr, sizeof(srv_addr));
-	srv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &srv_addr.sin_addr.s_addr, server->h_length);
-	srv_addr.sin_port = htons(portno);
-	if(connect(sockfd, (struct sockaddr *) &srv_addr, sizeof(srv_addr)) < 0) {
-		fprintf(stderr, "Error connecting to server\n");
-		exit(1);
-	}
-	bzero(buffer, 256);
-	strcpy(buffer, config.client.domain);
-	n = write(sockfd, buffer, strlen(buffer) +1);
-	if(n < 0) {
-		fprintf(stderr, "Error sending data.");
-		exit(1);
+	while(1) {
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if(sockfd < 0)
+			perror("Error opening socket");
+		server = gethostbyname(config.client.host);
+		if(server == NULL) {
+			fprintf(stderr, "Unknown host\n");
+			exit(1);
+		}
+		bzero((char *) &srv_addr, sizeof(srv_addr));
+		srv_addr.sin_family = AF_INET;
+		bcopy((char *) server->h_addr, (char *) &srv_addr.sin_addr.s_addr, server->h_length);
+		srv_addr.sin_port = htons(portno);
+		if(connect(sockfd, (struct sockaddr *) &srv_addr, sizeof(srv_addr)) < 0) {
+			fprintf(stderr, "Error connecting to server\n");
+			exit(1);
+		}
+		bzero(buffer, 256);
+		strcpy(buffer, config.client.domain);
+		n = write(sockfd, buffer, strlen(buffer) +1);
+		if(n < 0) {
+			fprintf(stderr, "Error sending data.");
+			exit(1);
+		}
+		close(sockfd);
+		sleep(30);
 	}
 	return 0;
 }

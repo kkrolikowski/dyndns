3deeb1317f76de621da82a82f106d3ed6a4f87ce
kkrolikowski <kkrolikowski@gmail.com>
2015-06-26 09:43:13 +0200

    db schema update issue: #28

--- dDNS-ng/doc/dbschema.sql
+++ dDNS-ng/doc/dbschema.sql
@@ -25,8 +25,28 @@ SET character_set_client = utf8;
 CREATE TABLE `domains` (
   `id` int(11) NOT NULL auto_increment,
   `domain` varchar(64) NOT NULL,
+  `status` varchar(7) NOT NULL default '',
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `mailqueue`
+--
+
+DROP TABLE IF EXISTS `mailqueue`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `mailqueue` (
+  `id` int(11) NOT NULL auto_increment,
+  `mailto` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `mailfrom` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `subject` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `reply_to` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `x_mailer` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `message` text character set utf8 collate utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -42,7 +62,7 @@ CREATE TABLE `user_log` (
   `ip` varchar(16) NOT NULL,
   `date` varchar(30) NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -54,18 +74,18 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
-  `login` varchar(16) NOT NULL,
-  `pass` varchar(36) NOT NULL,
-  `role` varchar(5) NOT NULL,
+  `login` varchar(16) collate utf8_unicode_ci NOT NULL,
+  `pass` varchar(36) collate utf8_unicode_ci NOT NULL,
+  `role` varchar(5) collate utf8_unicode_ci NOT NULL,
   `active` int(11) NOT NULL default '0',
-  `name` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `activate` varchar(24) NOT NULL,
-  `subdomain` varchar(64) NOT NULL,
-  `ip` varchar(16) NOT NULL,
-  `lastupdate` varchar(30) NOT NULL,
+  `name` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `email` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `activate` varchar(24) collate utf8_unicode_ci NOT NULL,
+  `subdomain` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `ip` varchar(16) collate utf8_unicode_ci NOT NULL,
+  `lastupdate` varchar(30) collate utf8_unicode_ci NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 SET character_set_client = @saved_cs_client;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -77,4 +97,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-06-01 21:41:53
+-- Dump completed on 2015-06-26  7:39:01
